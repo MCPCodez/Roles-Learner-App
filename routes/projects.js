8d647fd26df509906114bda8491c2029e33ab20e
MCPCodez <tidaldecode@gmail.com>
2024-04-24 18:26:59 -0500

    added some auth functionality based on users role

--- routes/projects.js
+++ routes/projects.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const { projects } = require('../data')
+const {authUser} = require('../basicAuth')
+const{canViewProject,scopedProjects} = require('../permissions/project')
 
-router.get('/', (req, res) => {
-  res.json(projects)
+router.get('/', authUser,(req, res) => {
+  res.json(scopedProjects(req.user, projects));
 })
 
-router.get('/:projectId', setProject, (req, res) => {
-  res.json(req.project)
+router.get('/:projectId', setProject, authUser, authGetProject,(req, res) => {
+  res.json(req.project);
 })
 
 function setProject(req, res, next) {
@@ -17,6 +19,14 @@ function setProject(req, res, next) {
   if (req.project == null) {
     res.status(404)
     return res.send('Project not found')
+  }
+  next()
+}
+
+function authGetProject(req,res,next){
+  if(!canViewProject(req.user,req.project)){
+    res.status(401);
+    return res.send('Not Allowed');
   }
   next()
 }
